568a43805ff79a6575f43697a69d10337ca8538d
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-06-28 21:04:09 +0000

    changed s3 to save multiple gems and install multiple gems, just need the json to be adjusted now

--- cookbooks/s3/recipes/get.rb
+++ cookbooks/s3/recipes/get.rb
@@ -1,15 +1,20 @@
 require 'aws'
 
-# get the name of the file and the bucket it's in to get it from S3
-bucket_name = node['s3-info']['bucket_name']
-file_name = node['s3-info']['file_name']
+# get the name of the files and their bucket to get it from S3
+
+#bucket_name = node['s3-info']['bucket_name']
+#file_name = node['s3-info']['file_name']
+
+s3_files = node['s3-info']
 
 s3 = AWS::S3.new
 
-# save the file
-save_file = File.open("#{node[:s3_folder_dir].last}/#{file_name}", 'w+')
-object = s3.buckets[bucket_name].objects[file_name]
-object.read do |chunk|
-  save_file.write(chunk)
+# save the files
+s3_files.each do |bucket_name, file_name|
+  save_file = File.open("#{node[:s3_folder_dir].last}/#{file_name}", 'w+')
+  object = s3.buckets[bucket_name].objects[file_name]
+  object.read do |chunk|
+    save_file.write(chunk)
+  end
+  save_file.close
 end
-save_file.close
--- cookbooks/s3/recipes/install_gem.rb
+++ cookbooks/s3/recipes/install_gem.rb
@@ -1,8 +1,11 @@
-file_name = node['s3-info']['file_name']
+s3_files = node['s3-info']
 
-# remove the gem name from the file name (remove the version number and ".gem"
-# from the end of the name)
-gem_name = /-[\d*\.]+\.gem$/.match(file_name).pre_match
-version = /[\d+\.]+\d/.match(/-[\d*\.]+\.gem$/.match(file_name).to_s).to_s
 
-`/usr/local/bin/gem install /opt/temp/#{file_name} -q --no-rdoc --no-ri -v "#{version}"`
+s3_files.each do |bucket,file_name|
+  # remove the gem name from the file name (remove the version number and ".gem"
+  # from the end of the name)
+  gem_name = /-[\d*\.]+\.gem$/.match(file_name).pre_match
+  version = /[\d+\.]+\d/.match(/-[\d*\.]+\.gem$/.match(file_name).to_s).to_s
+
+  `/usr/local/bin/gem install /opt/temp/#{file_name} -q --no-rdoc --no-ri -v "#{version}"`
+end
