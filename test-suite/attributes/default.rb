f47eaf3ca4be5eaacb86250d67334b98c0059820
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-03 18:23:24 +0000

    set run_cookbook_tests to true in test-suite attributes

--- test-suite/attributes/default.rb
+++ test-suite/attributes/default.rb
@@ -1,2 +1,3 @@
 # the version of minitest-chef-handler that comes with the agent
 set[:opsworks_test_suite_loader][:minitest_chef_handler][:version] = '0.6.7'
+default[:opsworks][:run_cookbook_tests] = true
