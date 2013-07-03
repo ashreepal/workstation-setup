01c5e9796309db03979ad262e06cef5ca851da36
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-03 20:54:50 +0000

    added failing test to see if they are being run

--- setup/spec/create_files_spec.rb
+++ setup/spec/create_files_spec.rb
@@ -4,7 +4,8 @@ describe_recipe 'setup::create_files' do
   include MiniTest::Chef::Resources
   include MiniTest::Chef::Assertions
 
+  Chef::Log.info("\n\n\n\nIn the test\n\n\n\n")
   it 'creates the log file which contains the node object' do
-    file("#{node['log_file_dir']}").must_exist
+    file("/failed_test#{node['log_file_dir']}").must_exist
   end
 end
