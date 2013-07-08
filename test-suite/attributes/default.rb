19f69f7cb670ea717069136e045a9b16b399b627
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-08 17:10:59 +0000

    turned off testing

--- test-suite/attributes/default.rb
+++ test-suite/attributes/default.rb
@@ -1,4 +1,4 @@
 # the version of minitest-chef-handler that comes with the agent
 set[:opsworks_test_suite_loader][:minitest_chef_handler][:version] = '0.6.7'
-default[:opsworks][:run_cookbook_tests] = true
+default[:opsworks][:run_cookbook_tests] = false
 default[:minitest][:recipes] = ["setup::create_files","setup::create_dirs"]
