f6287d55bfa4332f579c9e9d8482c34527967644
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-09 00:19:42 +0000

    trying a different way

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -2,15 +2,28 @@ node[:deploy].each do |application, deploy|
   deploy[:user] = 'ubuntu'
   deploy[:group] = 'ubuntu'
 
-  directory "/srv/www/#{application}/shared/cached-copy" do
-    owner 'root'
-    group 'root'
-    action :nothing
-    recursive true
-  end.run_action(:create)
+#  directory "/srv/www/#{application}/shared/cached-copy" do
+#    owner 'root'
+#    group 'root'
+#    action :nothing
+#    recursive true
+#  end.run_action(:create)
+  
+  
+  opsworks_deploy_user do
+    deploy_data deploy
+    app application
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
   
   opsworks_deploy do
     deploy_data deploy
     app application
   end
+
 end
