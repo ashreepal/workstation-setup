7661c2a6e5c24f56e91a0e8e600e653a82f80ccf
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-11 20:35:28 +0000

    no creds-related code will be run

--- setup/recipes/create_dirs.rb
+++ setup/recipes/create_dirs.rb
@@ -1,21 +1,21 @@
 # Creating directories for the log file and credentials file
 
 # creates all the directories into creds_folder_dir recursively
-creds_dir = directory "#{node['creds_folder_dir']}" do
-  mode '0755'
-  owner 'root'
-  group 'root'
-  action :nothing
-  recursive true
+#creds_dir = directory "#{node['creds_folder_dir']}" do
+#  mode '0755'
+#  owner 'root'
+#  group 'root'
+#  action :nothing
+#  recursive true
     
-  not_if do
-    ::File.exists?(node['creds_folder_dir'])
-  end
+#  not_if do
+#    ::File.exists?(node['creds_folder_dir'])
+#  end
 
-end
+#end
 
 # ensure that this happens immediately
-creds_dir.run_action(:create)
+#creds_dir.run_action(:create)
 
 # creates all the directories into log_folder_dir recursively
 log_dir = directory "#{node['log_folder_dir']}" do
