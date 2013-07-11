011ec93c19ebb1c24ded00736168d07ecb15a4bc
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-11 00:21:47 +0000

    fixed gem version functionality

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -25,9 +25,11 @@ end
 # if ruby 1.9.3 is installed, then install the gems (only after the configure stage)
 if /1\.9/.match(`ruby -v`)
   Chef::Log.info("\nRuby installed, so installing deploy gems\n")
-  node['install-on-deploy'].each do |g|
+  node['install-on-deploy'].each do |gem_info|
+    g,v = gem_info
     gem_package g do
       action :nothing
+      version v
       ignore_failure true
     end.run_action(:install)
   end
--- gems/recipes/default.rb
+++ gems/recipes/default.rb
@@ -1,8 +1,10 @@
 # install any public gems as directed through JSON
 
-node['install-on-setup'].each do |g|
+node['install-on-setup'].each do |gem_info|
+  g,v = gem_info
   gem_package g do
     ignore_failure true
+    version v
     action :nothing
   end.run_action(:install)
 end
