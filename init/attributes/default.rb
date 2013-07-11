a5e51b5d2bdb2e0139bdab98989f8853328f49bc
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-11 20:38:44 +0000

    removed default values for cred-related variables

--- init/attributes/default.rb
+++ init/attributes/default.rb
@@ -1,8 +1,8 @@
 # setting default values for the node object
 
 # credentials file location, to be created recursively
-default['creds_folder_dir'] = '/opt/aws'
-default['creds_file_dir'] = '/opt/aws/credentials.cfg'
+#default['creds_folder_dir'] = '/opt/aws'
+#default['creds_file_dir'] = '/opt/aws/credentials.cfg'
 
 # logfile location, to be created recursively
 default['log_folder_dir'] = '/opt/temp'
@@ -15,9 +15,9 @@ default['aws_folder_dir'] = '/opt/aws'
 default['s3_folder_dir'] = '/opt/temp'
 
 # default fake AWS-creds
-default['AWS-creds'] = { 'access_key_id' => 'access',
-                         'secret_access_key' => 'secret',
-                         'region' => 'us-east-1'          }
+#default['AWS-creds'] = { 'access_key_id' => 'access',
+#                         'secret_access_key' => 'secret',
+#                         'region' => 'us-east-1'          }
 
 # default fake bucket and file name to grab from s3
 default['s3-install-on-setup'] =
