f89628ddc15259b88bb1cdc6e295a64fe15f93f9
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-12 22:42:16 +0000

    Commented code - can now deploy and run code automatically

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -1,7 +1,5 @@
 # use opsworks definitions for deploying code from source (git, svn, etc.) to instance
 # by default, deploys to /srv/www/[app_name]/current
-require 'yaml'
-
 node[:deploy].each do |application, deploy|
   Chef::Log.info("\n\ndeploying stuff\n\n")
   deploy[:user] = 'ubuntu'
@@ -22,34 +20,29 @@ node[:deploy].each do |application, deploy|
     deploy_data deploy
     app application
   end
-
 end
 
+# if deploying the app, install the gems specified by the user and run
+# the code that is deployed
 if node[:opsworks][:activity] == 'deploy'
-  Chef::Log.info("\nmachine set up, so running deploy code\n")
-
+  
   node['install-on-deploy'].each do |gem_info|
     g,v = gem_info
     gem_package g do
       action :nothing
-#      version v
+      #version v
       ignore_failure true
     end.run_action(:install)
   end
 
-  node[:deploy].each do |application, deploy|
-    Chef::Log.info("1 #{deploy[:deploy_to]}/current/run.rb\n")
-    
+  node[:deploy].each do |application, deploy|    
     bash 'run_code' do
       code "sudo ruby #{deploy[:deploy_to]}/current/run.rb"
-      Chef::Log.info("\n\nFILE EXISTS: #{deploy[:deploy_to]}/current/run.rb")
-      Chef::Log.info("\n\nEXECUTED THE SUDO RUBY COMMAND")
         
       only_if do
         ::File.exists?("#{deploy[:deploy_to]}/current/run.rb")
       end
     end
   end
-else
-  Chef::Log.info("not yet set up, so will not run anything")
+
 end
