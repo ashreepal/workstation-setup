1a6509f2b651f935348c079df920b5b0a900cfe8
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-12 17:01:31 +0000

    allow code deploy on setup, but only run other deploy code on deploy command

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -1,28 +1,30 @@
 # use opsworks definitions for deploying code from source (git, svn, etc.) to instance
 # by default, deploys to /srv/www/[app_name]/current
+node[:deploy].each do |application, deploy|
+  Chef::Log.info("\n\ndeploying stuff\n\n")
+  deploy[:user] = 'ubuntu'
+  deploy[:group] = 'ubuntu'
+
+  opsworks_deploy_user do
+    deploy_data deploy
+    app application
+  end
+
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
+  
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+
+end
+
 if node[:opsworks][:activity] == 'deploy'
   Chef::Log.info("\nmachine set up, so running deploy code\n")
-  node[:deploy].each do |application, deploy|
-    deploy[:user] = 'ubuntu'
-    deploy[:group] = 'ubuntu'
-  
-    opsworks_deploy_user do
-      deploy_data deploy
-      app application
-    end
-  
-    opsworks_deploy_dir do
-      user deploy[:user]
-      group deploy[:group]
-      path deploy[:deploy_to]
-    end
-    
-    opsworks_deploy do
-      deploy_data deploy
-      app application
-    end
-  
-  end
 
   node['install-on-deploy'].each do |gem_info|
     g,v = gem_info
@@ -41,5 +43,5 @@ if node[:opsworks][:activity] == 'deploy'
     end
   end
 else
-  Chef::Log.info("not yet set up, so will not deploy, #{node['machine-set-up']}")
+  Chef::Log.info("not yet set up, so will not run anything, #{node['machine-set-up']}")
 end
