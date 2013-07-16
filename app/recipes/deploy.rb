72e0f695b764cf46de1c72a204c4d4f0105e4257
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-16 23:47:03 +0000

    removed logs

--- app/recipes/deploy.rb
+++ app/recipes/deploy.rb
@@ -30,24 +30,11 @@ if node[:opsworks][:activity] == 'deploy'
 
   config_options = {}
   config_options['activity-workers'] = node['activity-workers']
-  config_options = config_options['activity-workers'].map do |e|
-    Chef::Log.info("\n\n\nPrinting activity-workers information\n\n\n")
-    Chef::Log.info(e)
-    Chef::Log.info(e.class)
-    Chef::Log.info(e.to_hash)
-    Chef::Log.info(e.to_hash.class)
-    e.to_hash
-  end
+  config_options = config_options['activity-workers'].map { |e| e.to_hash }
+  
   config_options['workflow-workers'] = node['workflow-workers']
-  config_options = config_options['workflow-workers'].map do |e|
-    Chef::Log.info("\n\n\nPrinting workflow-workers information\n\n\n")
-    Chef::Log.info(e)
-    Chef::Log.info(e.class)
-    Chef::Log.info(e.to_hash)
-    Chef::Log.info(e.to_hash.class)
-    e.to_hash
-  end
-
+  config_options = config_options['workflow-workers'].map { |e| e.to_hash }
+  
   Chef::Log.info(config_options.to_yaml)
   
 
