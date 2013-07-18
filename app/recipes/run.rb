49c2bb3133bd1664851f7faa0e14264db5eee944
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-18 20:21:35 +0000

    using the runner gem

--- app/recipes/run.rb
+++ app/recipes/run.rb
@@ -1,9 +1,22 @@
+file '/home/ubuntu/runner.rb' do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  content 'require \'runner-gem\''
+  action :nothing
+
+  only_if do
+    ::File.exists?('/home/ubuntu/')
+  end
+
+end.run_action(:create)
+
 node[:deploy].each do |application, deploy|    
   bash 'run_code' do
-    code "sudo ruby #{deploy[:deploy_to]}/current/run.rb"
+    code "sudo ruby /home/ubuntu/runner.rb"
       
     only_if do
-      ::File.exists?("#{deploy[:deploy_to]}/current/run.rb")
+      ::File.exists?("/home/ubuntu/runner.rb")
     end
   end
 end
