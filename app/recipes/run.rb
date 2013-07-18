50df4e2e48d1455e2cd82d49294f3cf15d25d1c3
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-18 23:39:07 +0000

    changed runner file location

--- app/recipes/run.rb
+++ app/recipes/run.rb
@@ -1,5 +1,18 @@
+directory '/home/runner' do
+  mode '0755'
+  owner 'root'
+  group 'root'
+  action :nothing
+  recursive true
+  
+  not_if do
+    ::File.exists?('/home/runner')
+  end
+
+end.run_action(:create)
+
 # create the runner.rb file (just requires runner-gem
-file '/home/ubuntu/runner.rb' do
+file '/home/runner/runner.rb' do
   mode '0755'
   owner 'root'
   group 'root'
@@ -7,7 +20,7 @@ file '/home/ubuntu/runner.rb' do
   action :nothing
 
   only_if do
-    ::File.exists?('/home/ubuntu/')
+    ::File.exists?('/home/runner/')
   end
 
 end.run_action(:create)
@@ -15,10 +28,10 @@ end.run_action(:create)
 # run the code
 node[:deploy].each do |application, deploy|    
   bash 'run_code' do
-    code "sudo ruby /home/ubuntu/runner.rb"
+    code "sudo ruby /home/runner/runner.rb"
       
     only_if do
-      ::File.exists?("/home/ubuntu/runner.rb")
+      ::File.exists?("/home/runner/runner.rb")
     end
   end
 end
