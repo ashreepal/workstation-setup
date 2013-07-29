1cae35d830afe6a68634069a93fbd18f1c213fff
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-29 21:36:19 +0000

    added kill workers on undeploy

---
+++ app/recipes/stop.rb
@@ -0,0 +1,2 @@
+# kill the workers
+`pkill -9 -f #{node['runner_file_dir']}`
--- app/recipes/undeploy.rb
+++ app/recipes/undeploy.rb
@@ -1,2 +1,5 @@
 # uninstall gems on undeploy if they should be removed
 include_recipe 'app::uninstall_on_undeploy'
+
+# kill the workers
+include_recipe 'app::stop'
