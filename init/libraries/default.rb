f36b5f901afabd71b298528ebaae34f88342a81e
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-31 22:48:02 +0000

    moved functions to libraries/default

---
+++ init/libraries/default.rb
@@ -0,0 +1,38 @@
+module FileHelper
+  def new_dir(dir, mode, user, group)
+    new_dir = directory dir do
+      mode mode
+      owner user
+      group group
+      action :nothing
+      recursive true
+  
+      not_if do
+        ::File.exists?(dir)
+      end
+    end
+    
+    new_dir.run_action(:create)
+  end
+  
+  def new_file(dir, mode, user, group, content = nil, overwrite = false)
+    new_file = file dir do
+      mode mode
+      owner user
+      group group
+      action :nothing
+      content content
+  
+    end
+    
+    if overwrite
+      new_file.run_action(:create)
+    else
+      new_file.run_action(:create_if_missing)
+    end
+  end
+end
+
+class Chef::Recipe
+  include FileHelper
+end
