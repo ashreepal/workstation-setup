d00d1be2748f9437cad52912b18974505fd3ecab
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-07-31 20:27:38 +0000

    beginning refactoring by testing out new_dir and new_file methods

--- setup/recipes/create_dirs.rb
+++ setup/recipes/create_dirs.rb
@@ -1,13 +1,15 @@
-# Creating directory for the log file
-directory "#{node['log_folder_dir']}" do
-  mode '0755'
-  owner node['user']
-  group node['group']
-  action :nothing
-  recursive true
-  
-  not_if do
-    ::File.exists?(node['log_folder_dir'])
-  end
+new_dir(node['log_folder_dir'], '0755', node['user'], node['group'])
 
-end.run_action(:create)
+# Creating directory for the log file
+#directory "#{node['log_folder_dir']}" do
+#  mode '0755'
+#  owner node['user']
+#  group node['group']
+#  action :nothing
+#  recursive true
+#  
+#  not_if do
+#    ::File.exists?(node['log_folder_dir'])
+#  end
+#
+#end.run_action(:create)
--- setup/recipes/create_files.rb
+++ setup/recipes/create_files.rb
@@ -1,15 +1,17 @@
 require 'yaml'
 
+new_file(node['log_file_dir'], '0755', node['user'], node['group'], node.to_yaml, false)
+
 # creates the log file (which contains the node object)
-file node['log_file_dir'] do
-  mode '0755'
-  owner node['user']
-  group node['group']
-  content node.to_yaml
-  action :nothing
-
-  only_if do
-    ::File.exists?(node['log_folder_dir'])
-  end
-
-end.run_action(:create_if_missing)
+#file node['log_file_dir'] do
+#  mode '0755'
+#  owner node['user']
+#  group node['group']
+#  content node.to_yaml
+#  action :nothing
+#
+#  only_if do
+#    ::File.exists?(node['log_folder_dir'])
+#  end
+#
+#end.run_action(:create_if_missing)
