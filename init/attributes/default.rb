bb6f042b467013de08389b49048505ffe7081d7b
Shreepal Shah <ubuntu@ip-10-152-178-112.ec2.internal>
2013-08-01 18:18:49 +0000

    quick update before code review, removed old code

--- init/attributes/default.rb
+++ init/attributes/default.rb
@@ -46,38 +46,3 @@ default['runner_file_dir'] = "#{node['runner_folder_dir']}/runner.rb"
 
 # default name for each worker (once the process is forked)
 default['worker_process_name'] = 'aws-rf-worker'
-
-# Commonly used functions
-
-def new_dir(dir, mode, user, group)
-  new_dir = directory dir do
-    mode mode
-    owner user
-    group group
-    action :nothing
-    recursive true
-
-    not_if do
-      ::File.exists?(dir)
-    end
-  end
-  
-  new_dir.run_action(:create)
-end
-
-def new_file(dir, mode, user, group, content = nil, overwrite = false)
-  new_file = file dir do
-    mode mode
-    owner user
-    group group
-    action :nothing
-    content content
-
-  end
-  
-  if overwrite
-    new_file.run_action(:create)
-  else
-    new_file.run_action(:create_if_missing)
-  end
-end
